212c9f3ba3ca0f521e8353693973d6f60c643c6e
Vikram05vikky <125629713+Vikram05vikky@users.noreply.github.com>
2023-11-03 10:51:30 +0530

    Create LeetCode_Valid Anagram.cpp

---
+++ Day 3/LeetCode_Valid Anagram.cpp
@@ -0,0 +1,14 @@
+Problem : 
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Input :
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
