41bb413d5b05459267549539e03e7b73ab8ead8d
Vikram05vikky <125629713+Vikram05vikky@users.noreply.github.com>
2023-11-05 14:47:11 +0530

    Update LeetCode_Maximum Product of Three Numbers.cpp

--- Day 5/LeetCode_Maximum Product of Three Numbers.cpp
+++ Day 5/LeetCode_Maximum Product of Three Numbers.cpp
@@ -1,1 +1,22 @@
+Problem : Maximum Product of Three Numbers
+
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 6
+
+Input:
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int prod=1;
+        
+        return max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
+    }
+};
 
