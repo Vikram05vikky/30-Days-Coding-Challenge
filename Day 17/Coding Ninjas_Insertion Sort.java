751241599587a39a768645cf325065985225ea8d
Vikram05vikky <125629713+Vikram05vikky@users.noreply.github.com>
2023-11-17 16:23:00 +0530

    Create Coding Ninjas_Insertion Sort.java

---
+++ Day 17/Coding Ninjas_Insertion Sort.java
@@ -0,0 +1,13 @@
+Problem: Insertion Sort
+
+Input:
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+	public static void insertionSort(int n , int[] arr) {
+		Arrays.sort(arr);
+	}
+}
+
