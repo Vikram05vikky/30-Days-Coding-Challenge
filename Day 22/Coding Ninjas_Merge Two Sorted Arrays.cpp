fd4c71a6ce311bc3cf192ec0e9fc7a9bfd0ff15b
Vikram05vikky <125629713+Vikram05vikky@users.noreply.github.com>
2023-11-22 18:03:31 +0530

    Create Coding Ninjas_Merge Two Sorted Arrays.cpp

---
+++ Day 22/Coding Ninjas_Merge Two Sorted Arrays.cpp
@@ -0,0 +1,14 @@
+Problem : Merge Two Sorted Arrays
+
+Input :
+#include <bits/stdc++.h>
+
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	// Write your code here.
+	 sort(arr1.begin(),arr1.end());
+    for(int i=0;i<n;i++){
+        arr1[i]=arr2[i];
+    }
+    sort(arr1.begin(),arr1.end());
+    return arr1;
+}
