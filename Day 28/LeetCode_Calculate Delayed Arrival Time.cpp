95d90d9378a0ea37aea3d720e64b7e2381a4a059
Vikram05vikky <125629713+Vikram05vikky@users.noreply.github.com>
2023-11-28 18:10:54 +0530

    Create LeetCode_Calculate Delayed Arrival Time.cpp

---
+++ Day 28/LeetCode_Calculate Delayed Arrival Time.cpp
@@ -0,0 +1,12 @@
+Problem : Calculate Delayed Arrival Time
+
+Input :
+
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        
+        int ans=arrivalTime+delayedTime;
+        return ans%24;
+    }
+};
