0c9de80245c8bedea9d9eba8cf8e5196f3e76b3a
Vikram05vikky <125629713+Vikram05vikky@users.noreply.github.com>
2023-11-29 13:50:36 +0530

    Create CodeChef_Peaceful Party.cpp

---
+++ Day 29/CodeChef_Peaceful Party.cpp
@@ -0,0 +1,21 @@
+Problem : Peaceful Party
+
+Input :
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+c >= b){
+            cout<<(a+c)<<endl;
+        }
+        else{
+            cout<<b<<endl;
+        }
+    }
+	return 0;
+}
